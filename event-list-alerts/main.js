80e6f1d60f05c2b8693d62b9e4de0fc05cde54aa
Græ <61520887+Verge73@users.noreply.github.com>
2023-12-15 21:33:36 +0100

    Update main.js

--- event-list-alerts/main.js
+++ event-list-alerts/main.js
@@ -374,7 +374,7 @@ function doAlert() {
 				alertAudioElem[0].play();
 			}
 		}
-
+/*
 		if(alertData.doTTS) {
 			const req = new XMLHttpRequest()
 			req.open('POST', `https://tiktok-tts.weilnet.workers.dev/api/generation`, false)
@@ -403,6 +403,47 @@ function doAlert() {
 			}
 		} else {
 			processAlertsTO = setTimeout(processAlerts, 7000);
+		}
+	*/
+
+				if (alertData.doTTS) {
+		  const req = new XMLHttpRequest();
+		  req.open('POST', `https://tiktok-tts.weilnet.workers.dev/api/generation`, false);
+		  req.setRequestHeader('Content-Type', 'application/json');
+		  req.send(JSON.stringify({
+		    text: `${alertData.name === "" ? alertData.username : alertData.name} ${alertMsgElem.text()}`,
+		    voice: "en_us_001"
+		  }));
+		
+		  let resp = JSON.parse(req.responseText);
+		  console.log(resp);
+		
+		  if (resp.data === null) {
+		    setTimeout(() => processAlerts(), 7000);
+		  } else {
+		    ttsAudioElem[0].src = `data:audio/mpeg;base64,${resp.data}`;
+		    ttsAudioElem[0].volume = 0.67;
+		
+		    let ttsAudioLoaded = false;
+		
+		    ttsAudioElem[0].addEventListener('canplaythrough', () => {
+		      if (!ttsAudioLoaded) {
+		        ttsAudioLoaded = true;
+		
+		        // Delay 3.5 seconds before playing the audio
+		        setTimeout(() => ttsAudioElem[0].play(), 3500);
+		      }
+		    });
+		
+		    ttsAudioElem[0].addEventListener('ended', () => {
+		      console.log('ENDED');
+		
+		      // Delay 2.5 seconds before processing the next alert
+		      setTimeout(() => processAlerts(), 2500);
+		    });
+		  }
+		} else {
+		  setTimeout(() => processAlerts(), 7000);
 		}
 	});
 }
