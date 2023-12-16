b923474f815a5608abea7803595e7e6ec044fa2c
Græ <61520887+Verge73@users.noreply.github.com>
2023-12-16 14:10:28 +0100

    Update test.js

--- event-list-alerts/test.js
+++ event-list-alerts/test.js
@@ -1,5 +1,13 @@
-HttpResponse<String> response = Unirest.post("https://api.elevenlabs.io/v1/text-to-speech/PmxyxhMMr4p99dBVCopY")
-  .header("xi-api-key", "5416bfbffae6ad12a9e1c4ad6a0b1b51")
-  .header("Content-Type", "application/json")
-  .body("{\n  \"text\": \"aaaaaaaaaaaaa\"\n}")
-  .asString();
+const options = {
+  method: 'POST',
+  headers: {
+    'xi-api-key': '5416bfbffae6ad12a9e1c4ad6a0b1b51',
+    'Content-Type': 'application/json'
+  },
+  body: '{"text":"aaaaaaaaaaaaa"}'
+};
+
+fetch('https://api.elevenlabs.io/v1/text-to-speech/PmxyxhMMr4p99dBVCopY', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
