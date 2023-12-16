8bbbb2dc9a7221c4f95acc2ba0a0d31830571111
Græ <61520887+Verge73@users.noreply.github.com>
2023-12-16 14:19:31 +0100

    Update test.js

--- event-list-alerts/test.js
+++ event-list-alerts/test.js
@@ -4,10 +4,10 @@ const options = {
     'xi-api-key': '5416bfbffae6ad12a9e1c4ad6a0b1b51',
     'Content-Type': 'application/json'
   },
-  body: '{"text":"aaaaaaaaaaaaa"}'
+  body: '{"text":"and it is exactly why we should think of another solution that doesn\'t involve this middling approach of...","model_id":"eleven_multilingual_v2"}'
 };
 
-fetch('https://api.elevenlabs.io/v1/text-to-speech/PmxyxhMMr4p99dBVCopY', options)
+fetch('https://api.elevenlabs.io/v1/text-to-speech/PmxyxhMMr4p99dBVCopY?optimize_streaming_latency=1&output_format=mp3_44100_128', options)
   .then(response => response.json())
   .then(response => console.log(response))
   .catch(err => console.error(err));
