3a568323798bd02c3f152dc8545913c1c7c9dc8a
Græ <61520887+Verge73@users.noreply.github.com>
2023-12-16 22:47:55 +0100

    Update main.js

--- event-list-alerts/main.js
+++ event-list-alerts/main.js
@@ -374,7 +374,7 @@ function doAlert() {
 				alertAudioElem[0].play();
 			}
 		}
-	
+	/*
 if(alertData.doTTS) {
 			const req = new XMLHttpRequest()
 			req.open('POST', `https://tiktok-tts.weilnet.workers.dev/api/generation`, false)
@@ -405,7 +405,25 @@ if(alertData.doTTS) {
 			processAlertsTO = setTimeout(processAlerts, 7000);
 		}
 
+*/
 
+		const options = {
+  method: 'POST',
+  headers: {
+    'xi-api-key': '5416bfbffae6ad12a9e1c4ad6a0b1b51',
+    'Content-Type': 'application/json'
+  },
+  body: '{"text":"and it is exactly why we should think of another solution that doesn\'t involve this middling approach of...","model_id":"eleven_multilingual_v2"}'
+};
+
+fetch('https://api.elevenlabs.io/v1/text-to-speech/PmxyxhMMr4p99dBVCopY?optimize_streaming_latency=1&output_format=mp3_44100_128', options)
+  .then(response => response.blob())
+  .then(blob => {
+    const url = URL.createObjectURL(blob);
+    const audio = new Audio(url);
+    audio.play();
+  })
+  .catch(err => console.error(err));
 		
 		
 
